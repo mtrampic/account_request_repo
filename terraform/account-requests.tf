697a3803950adbcd7490d09640afaa4d4eefa829
Mladen Trampic <mladen@trampic.info>
2024-02-13 15:32:40 +0000

    Creating new AWS account

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -33,3 +33,40 @@ module "account_request_01" {
 
   account_customizations_name = "SANDBOX"
 }
+
+
+module "account_request_02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "trampic+ct-immersion-day-aft-sp-2@amazon.com"
+    AccountName               = "Spoke-2"
+    ManagedOrganizationalUnit = "Labs (ou-zym3-q5k8fu3x)" 
+    SSOUserEmail              = "trampic+ct-immersion-day-aft-sp-2@amazon.com"
+    SSOUserFirstName          = "Mladen"
+    SSOUserLastName           = "Trampic"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "mladen@trampic.infp"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
